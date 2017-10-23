81d7081a3137119ad7206b7904ddbad7479baf03
tuapuikia <dknyvice@gmail.com>
2017-10-23 19:35:52 +0800

    use template for user data

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_launch_configuration" "launch_config" {
     iam_instance_profile = "${var.iam_instance_profile}"
     key_name = "${var.key_name}"
     security_groups = ["${var.security_group}"]
-    user_data = "${file(var.user_data)}"
+    user_data = "${var.user_data}"
 }
 
 resource "aws_autoscaling_group" "main_asg" {
